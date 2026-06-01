2a46b82115ae711ea6b53b5d778bcdc924ac98e3
sonacbi <sonacbi0@gmail.com>
2026-06-01 17:18:58 +0900

    hook : 페이지 상태관리 분리

---
+++ seeve/src/hooks/useStudyDeskState.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+
+function useStudyDeskState() {
+
+    // 불러온 pdf 파일 -> preview와 screen에 호출할 데이터
+    const [pdfFile, setPdfFile] = useState(null);
+    
+    // 챕터(pdf 페이지 수) 표기용. 안전장치로 최소 1페이지 세팅
+    const [lectureCount, setLectureCount] = useState(1);
+    const MAX_NOTE_PAGE = 5; // 한 챕터당 최대 생성가능 페이지 수
+    
+    // 챕터(pdf 페이지 수)당 배정된 노트 표기용. 안전장치로 최소 1노트 세팅
+    const [currentNoteId, setCurrentNoteId] = useState("p1-1");
+    
+    // 초기 데이터 생성. 안전장치로 최소 1노트 세팅
+    const createInitialPages = ( count ) => {
+        const pages = {};
+
+        for ( let i = 1; i <= count; i++ ) {
+            pages[`p${i}`] = [{
+                id: `p${i}-1`,
+                content: "",
+                },
+            ];
+        }
+
+        return pages;
+    };
+    
+    // 노트페이지 세팅 (pdfview > document에서 수신한 페이지를 근거로 LectureCount를 setting)
+    const [notePages, setNotePages] =
+        useState(() => createInitialPages(lectureCount));
+    
+    // professor 내비게이션의 보이는 순서 세팅. 기본적으로 i의 갯수는 pdf 페이지의 n-1과 같다.
+    const [professorOrder, setProfessorOrder] =
+        useState( Array.from( { length: lectureCount },
+                (_, i) => `p${i + 1}`
+            )
+        );
+        
+    // 내비게이션 정렬된대로 도면을 그리며 맵을 만든다.
+    const flattenedNotes = professorOrder.flatMap((lecturePage) => {
+            const notes = notePages[ lecturePage ] ?? [];
+
+            return notes.map( (note) => ({
+                lecturePage,
+                ...note,
+            }));
+        });
+    
+    
+    useEffect(() => {
+        console.log(lectureCount);
+        setNotePages( (prev) => {
+
+            const next = { ...prev };
+
+            for ( let i = 1; i <= lectureCount; i++ ) { // pdf 페이지수만큼 노트 생성 (기본값)
+                const key = `p${i}`;
+                if ( !next[key] ) {
+                    next[key] = [{
+                        id: `${key}-1`,
+                        content: "",
+                    },];
+                }
+            }
+            
+            return next;
+        });
+
+        setProfessorOrder(
+            Array.from(
+                { length: lectureCount },
+                (_, i) => `p${i + 1}`
+            )
+        );
+
+    }, [lectureCount]);
+
+    const totalNoteCount = /*flattenedNotes.length;*/
+        flattenedNotes.filter(
+            note => note.content.trim().length > 0
+        ).length;
+    
+    // 현재 노트 id를 갖고 flattend 맵에서 찾는다. 없다면 첫페이지를 반환
+    const currentNote = flattenedNotes.find( (note) => note.id === currentNoteId ) ?? flattenedNotes[0];
+    // 현재 pdf에 대응되는 현 노트
+    const currentLectureNotes = currentNote
+            ? notePages[currentNote.lecturePage]
+            : [];
+
+    // 현재 키값에 대응하는 데이터값에서 "p"를 제외하고 슬라이싱
+    const currentLectureIndex = currentNote
+        ? parseInt(currentNote.lecturePage.replace("p", ""))
+        : 1;
+
+    return {
+        pdfFile, setPdfFile,
+        lectureCount, setLectureCount,
+        MAX_NOTE_PAGE,
+        currentNoteId, setCurrentNoteId,
+        createInitialPages,
+        notePages, setNotePages,
+        professorOrder, setProfessorOrder,
+        flattenedNotes,
+        totalNoteCount,
+        currentNote,
+        currentLectureNotes,
+        currentLectureIndex,
+    }
+}
+
+export default useStudyDeskState
--- seeve/src/pages/StudyDeskPage.jsx
+++ seeve/src/pages/StudyDeskPage.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
+import useStudyDeskState from "../hooks/useStudyDeskState"
 import ProfessorPanel from "../components/professor/ProfessorPanel";
 import NotePanel from "../components/note/NotePanel"
 import useRollback from "../hooks/useRollback";
@@ -8,190 +9,20 @@ import FloatNavigator from "../components/FloatNavigator/FloatNavigator";
 import colorPalette from "../styles/color";
 
 function StudyDeskPage() {
-    const [pdfFile, setPdfFile] =
-        useState(null);
-
-    // 챕터표기용 가안
-    const [lectureCount,
-        setLectureCount] =
-        useState(1);
-    const MAX_NOTE_PAGE = 5;
-    
-    
-    // 초기 데이터 생성
-    const createInitialPages = (
-        count
-    ) => {
-        const pages = {};
-
-        for (
-            let i = 1;
-            i <= count;
-            i++
-        ) {
-            pages[`p${i}`] = [
-                {
-                    id:
-                        `p${i}-1`,
-                    content: "",
-                },
-            ];
-        }
-
-        return pages;
-    };
-
-    const [notePages,
-        setNotePages] =
-        useState(
-            () =>
-                createInitialPages(
-                    lectureCount
-                )
-        );
-    useEffect(() => {
-        setNotePages(
-            (prev) => {
-
-                const next =
-                    { ...prev };
-
-                for (
-                    let i = 1;
-                    i <= lectureCount;
-                    i++
-                ) {
-
-                    const key =
-                        `p${i}`;
-
-                    if (
-                        !next[key]
-                    ) {
-                        next[key] = [
-                            {
-                                id:
-                                    `${key}-1`,
-                                content:
-                                    "",
-                            },
-                        ];
-                    }
-                }
-
-                return next;
-            }
-        );
-
-        setProfessorOrder(
-            Array.from(
-                {
-                    length:
-                        lectureCount
-                },
-                (_, i) =>
-                    `p${i + 1}`
-            )
-        );
-
-    }, [lectureCount]);
-            // const [currentNoteIndex, setCurrentNoteIndex] =
-        //     useState(0);
-
-    useEffect(() => {
-        console.log(lectureCount);
-
-        setNotePages((prev) => {
-            const next = { ...prev };
-
-            for (
-                let i = 1;
-                i <= lectureCount;
-                i++
-            ) {
-                const key = `p${i}`;
-
-                if (!next[key]) {
-                    next[key] = [
-                        {
-                            id: `${key}-1`,
-                            content: "",
-                        },
-                    ];
-                }
-            }
-
-            console.log(next);
-
-            return next;
-        });
-
-    }, [lectureCount]);
-    
-    const [currentNoteId, setCurrentNoteId] =
-        useState("p1-1");
-
-    // flat 구조 professor 페이지 대로.
-    // const flattenedNotes = Object.entries(
-    //     notePages
-    // ).flatMap(([lecturePage, notes]) =>
-        //     notes.map((note) => ({
-            //     lecturePage,
-            //     ...note,
-            //     }))
-            // );
-    const [professorOrder,
-        setProfessorOrder] =
-        useState(
-            Array.from(
-                { length: lectureCount },
-                (_, i) => `p${i + 1}`
-            )
-    );
-    // 내비게이션 정렬된대로
-    const flattenedNotes =
-        professorOrder.flatMap(
-            (lecturePage) => {
-                    
-            const notes =
-                notePages[
-                    lecturePage
-                ] ?? [];
-
-            return notes.map(
-                (note) => ({
-                    lecturePage,
-                    ...note,
-                })
-            );
-        }
-    );
-    const totalNoteCount = /*flattenedNotes.length;*/
-        flattenedNotes.filter(
-            note => note.content.trim().length > 0
-        ).length;
-
-    const currentNote =
-        // flattenedNotes[currentNoteIndex] ?? flattenedNotes[0];
-        flattenedNotes.find(
-            (note) => note.id === currentNoteId
-        ) ?? flattenedNotes[0];
-
-
-    // 현재 교수페이지의 노트
-    const currentLectureNotes =
-        // notePages[currentNote.lecturePage];
-        currentNote
-            ? notePages[currentNote.lecturePage]
-            : [];
-
-    // 현재 데이터 표기
-    // const currentLectureIndex = parseInt(
-    //   currentNote.lecturePage.replace("p", "")
-    // );
-    const currentLectureIndex = currentNote
-        ? parseInt(currentNote.lecturePage.replace("p", ""))
-        : 1;
+    const {
+        pdfFile, setPdfFile,
+        lectureCount, setLectureCount,
+        MAX_NOTE_PAGE,
+        currentNoteId, setCurrentNoteId,
+        // createInitialPages,
+        notePages, setNotePages,
+        professorOrder, setProfessorOrder,
+        flattenedNotes,
+        totalNoteCount,
+        currentNote,
+        currentLectureNotes,
+        currentLectureIndex,
+    } = useStudyDeskState();
 
     const rollback = useRollback();
 
@@ -216,13 +47,7 @@ function StudyDeskPage() {
 
     const [isDark, setIsDark] = useState(false); // 테스트용 코드
     const { background, c_background } = colorPalette(isDark);
-// console.log(mode);
-// useEffect(() => {
-//     console.log(
-//         "lectureCount:",
-//         lectureCount
-//     );
-// }, [lectureCount]);
+
     return (
 
         <div id="wrapper"
